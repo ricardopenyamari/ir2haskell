4e17484f5a23259fc2484d64651e5e65d19c869e
ricardopenyamari <ricardo@sip.ucm.es>
2017-05-18 17:31:53 +0200

    Add files via upload

---
+++ clir-parser-haskell-master/uutHeader.hs
@@ -0,0 +1,12 @@
+
+module UUT where
+
+
+import qualified DataStructures.Arrays as A
+import qualified DataStructures.Bags as B
+import qualified DataStructures.Sets as S
+import qualified DataStructures.Sequences as Q
+import DataStructures.Assertion
+
+
+
